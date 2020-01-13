942c44e859ea9813b96e0db68e90a5aef0e12a4f
Jonathan Gibbons <gibbons_jonathan@hotmail.com>
2020-01-13 18:05:46 +0000

    Added debug and ignore cert authority config items

--- src/jgibbons.com/goxmlfilter/config/filterconfig.go
+++ src/jgibbons.com/goxmlfilter/config/filterconfig.go
@@ -8,13 +8,15 @@ import (
 )
 
 type FilterConfig struct {
-	RowsInFile       int            `json:"rows_in_file"`
-	NumFiles         int            `json:"num_files"`
-	RestQuery        string         `json:"query"`
-	DelimTag         string         `json:"first_tag"`
-	FiltersEquals    []FilterField  `json:"filter_equals"`
-	FiltersNotEquals []FilterField  `json:"filter_not_equals"`
-	ExtractColumns   []ExtractField `json:"filter_extract"`
+	RowsInFile         int            `json:"rows_in_file"`
+	NumFiles           int            `json:"num_files"`
+	RestQuery          string         `json:"query"`
+	IgnoreCertAuthoriy bool           `json:"ignore_cert_authority"`
+	DelimTag           string         `json:"first_tag"`
+	DebugOutput        bool           `json:"debug_output"`
+	FiltersEquals      []FilterField  `json:"filter_equals"`
+	FiltersNotEquals   []FilterField  `json:"filter_not_equals"`
+	ExtractColumns     []ExtractField `json:"filter_extract"`
 }
 
 type FilterField struct {
@@ -44,9 +46,12 @@ func New(fileName string) (*FilterConfig, error) {
 
 func (data *FilterConfig) DumpConfig() {
 	fmt.Printf("URL [%s]\n", data.RestQuery)
+	fmt.Printf("IgnoreCertAuthority [%t]\n", data.IgnoreCertAuthoriy)
 	fmt.Printf("RowsInFile [%d]\n", data.RowsInFile)
 	fmt.Printf("NumFiles [%d]\n", data.NumFiles)
 	fmt.Printf("DelimTag [%s]\n", data.DelimTag)
+	fmt.Printf("DebugOutput [%t]\n", data.DebugOutput)
+
 	fmt.Printf("Filters:\n")
 	for i := 0; i < len(data.FiltersEquals); i++ {
 		f := data.FiltersEquals[i]
